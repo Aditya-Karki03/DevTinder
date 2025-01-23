a1a8deedf8be221f8e685bf77e4917d769ff5d94
Aditya Karki <adityakarki201@gmail.com>
2025-01-23 22:31:04 +0530

    Simple server with login route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hellow orld");
+import express from "express";
+import { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+app.use("/login", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log("Port is listening at " + port);
+});
