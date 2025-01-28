80185b270b146aa01011e71a6cfa3cef41670c8f
Aditya Karki <adityakarki201@gmail.com>
2025-01-28 13:55:04 +0530

    DB connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { Request, Response } from "express";
+import { connectDB, disconnectDB } from "./config/db";
+import { config } from "dotenv";
+config();
 const app = express();
 const port = 3000;
 
@@ -7,6 +10,12 @@ app.use("/login", (req: Request, res: Response) => {
   res.send("Hello world");
 });
 
-app.listen(port, () => {
-  console.log("Port is listening at " + port);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Port is listening at", port);
+    });
+  })
+  .catch((error) => console.log(error));
+
+//create a config folder to put every configuration files inside of it
