b24aa80dbc6946c5c534aded12a07f5bd9eb36c8
Aditya Karki <adityakarki201@gmail.com>
2025-02-04 14:08:02 +0530

    Remove backend add tailwind & daisy UI

--- src/utils/tokenGenerator.ts
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const tokenGenerator = (userId: string) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET || "", {
-    expiresIn: "1d",
-  });
-  return token;
-};
