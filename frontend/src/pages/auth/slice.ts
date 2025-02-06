4786a7bf6aef34d75829aa67b72044d74ecf862e
Aditya Karki <adityakarki201@gmail.com>
2025-02-06 23:13:58 +0530

    Add slice and reducers to the store

---
+++ frontend/src/pages/auth/slice.ts
@@ -0,0 +1,49 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IUser {
+  _id: string;
+  email: string;
+}
+
+interface loginState {
+  user: IUser | null;
+  isLoggedIn: boolean;
+  loading: boolean;
+  error: null | string;
+}
+
+const initialState: loginState = {
+  user: null,
+  isLoggedIn: false,
+  loading: false,
+  error: null,
+};
+
+const loginUserSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginRequest: (state) => {
+      state.loading = true;
+      //remove any previous error
+      state.error = null;
+    },
+    loginSuccessfull: (state, action: PayloadAction<IUser>) => {
+      state.loading = false;
+      state.isLoggedIn = true;
+      state.user = action.payload;
+    },
+    loginFail: (state, action: PayloadAction<string>) => {
+      state.loading = false;
+      state.isLoggedIn = false;
+      state.error = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.isLoggedIn = false;
+    },
+  },
+});
+export const { loginRequest, loginSuccessfull, loginFail, logout } =
+  loginUserSlice.actions;
+export default loginUserSlice.reducer;
---
+++ frontend/src/redux/reducers.ts
@@ -0,0 +1,5 @@
+import userReducer from "../pages/auth/slice";
+const reducers = {
+  auth: userReducer,
+};
+export default reducers;
