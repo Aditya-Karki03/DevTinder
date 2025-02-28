bed4a6e71a6307bc2eb39fbdd4027e0886f7c75e
Aditya Karki <adityakarki201@gmail.com>
2025-02-28 09:32:06 +0530

    Add validation for the edit profile page

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CircleX } from "lucide-react";
 import { useForm } from "react-hook-form";
-import { modifyFormSchema } from "../schema/schema";
+import { modifyFormSchema, modifyFormSchemaType } from "../schema/schema";
 
 interface ModalProps {
   setShowModal: () => void;
@@ -14,11 +14,15 @@ const Modal = ({ setShowModal }: ModalProps) => {
     register,
     formState: { errors },
     getValues,
-  } = useForm({
+  } = useForm<modifyFormSchemaType>({
     resolver: zodResolver(modifyFormSchema),
   });
+
+  const onSubmit = (data: modifyFormSchemaType) => {
+    console.log(data);
+  };
   return (
-    <div className="min-w-5xl h-8/10 flex justify-center items-center absolute z-50 top-[10%] left-[50%] translate-x-[-50%] shadow-2xl">
+    <div className="min-w-5xl flex justify-center items-center absolute z-50 top-[5%] left-[50%] translate-x-[-50%] shadow-2xl">
       <div className="w-full h-full bg-white rounded-3xl">
         <div className="flex w-full justify-end p-3 ">
           <CircleX
@@ -28,54 +32,116 @@ const Modal = ({ setShowModal }: ModalProps) => {
         </div>
         <div className="grid grid-cols-2 h-full">
           <div className="text-black border-dashed border-r-2  h-full">
-            <form className="px-4 space-y-4">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="px-4 space-y-2 overflow-y-scroll pb-4"
+            >
               <div className="">
                 <label htmlFor="firstName">Firstname: </label>
                 <input
                   type="text"
-                  className="py-2 px-1 rounded-md outline-none w-full text-md"
+                  {...register("firstName")}
+                  className={`py-2 px-1 rounded-md outline-none w-full text-md ${
+                    errors.firstName && "border border-red-500"
+                  }`}
                 />
+                {errors?.firstName && (
+                  <p className="text-sm text-red-500">
+                    {errors?.firstName?.message}
+                  </p>
+                )}
               </div>
               <div className="">
-                <label htmlFor="firstName">Lastname: </label>
+                <label htmlFor="lastName">Lastname: </label>
                 <input
                   type="text"
-                  className="py-2 px-1 rounded-md outline-none w-full text-md"
+                  {...register("lastName")}
+                  className={`py-2 px-1 rounded-md outline-none w-full text-md ${
+                    errors?.lastName && "border border-red-500"
+                  }`}
                 />
+                {errors?.lastName && (
+                  <p className="text-sm text-red-500">
+                    {errors?.lastName?.message}
+                  </p>
+                )}
               </div>
               <div className="">
-                <label htmlFor="firstName">Age: </label>
+                <label htmlFor="age">Age: </label>
                 <input
                   type="number"
-                  className="py-2 px-1 rounded-md outline-none w-full text-md"
+                  {...register("age")}
+                  className={`py-2 px-1 rounded-md outline-none w-full text-md ${
+                    errors.age && "border border-red-500"
+                  }`}
                 />
+                {errors?.age && (
+                  <p className="text-sm text-red-500">{errors?.age?.message}</p>
+                )}
               </div>
               <div className="">
-                <label htmlFor="firstName">Gender: </label>
+                <label htmlFor="gender">Gender: </label>
                 <input
                   type="text"
-                  className="py-2 px-1 rounded-md outline-none w-full text-md"
+                  {...register("gender")}
+                  className={`py-2 px-1 rounded-md outline-none w-full text-md ${
+                    errors?.gender && "border border-red-500"
+                  }`}
                 />
+                {errors?.gender && (
+                  <p className="text-sm text-red-500">
+                    {errors?.gender?.message}
+                  </p>
+                )}
               </div>
               <div className="">
-                <label htmlFor="firstName">Skills: </label>
+                <label htmlFor="skills">Skills: </label>
                 <input
                   type="text"
-                  className="py-2 px-1 rounded-md outline-none w-full text-md"
+                  {...register("skills")}
+                  className={`py-2 px-1 rounded-md outline-none w-full text-md ${
+                    errors?.skills && "border border-red-500"
+                  }`}
                 />
+                {errors?.firstName && (
+                  <p className="text-sm text-red-500">
+                    {errors?.skills?.message}
+                  </p>
+                )}
               </div>
               <div className="">
-                <label htmlFor="firstName">About: </label>
-                <textarea className="py-2 px-1 rounded-md outline-none w-full text-md" />
+                <label htmlFor="about">About: </label>
+                <textarea
+                  {...register("about")}
+                  className={`py-2 px-1 rounded-md outline-none w-full text-md ${
+                    errors?.about && "border border-red-500"
+                  }`}
+                />
+                {errors?.about && (
+                  <p className="text-sm text-red-500">
+                    {errors?.about?.message}
+                  </p>
+                )}
               </div>
               <div className="w-full h-full">
+                <label htmlFor="image">Profile Photo:</label>
                 <input
                   type="file"
-                  name=""
-                  id=""
-                  className="border border-dashed w-full h-full rounded-md py-8 px-7"
+                  {...register("image")}
+                  className="border border-dashed w-full h-full rounded-md py-6 px-7"
                 />
+                {errors?.image && (
+                  <p className="text-sm text-red-500">
+                    {errors?.image?.message?.toString()}
+                  </p>
+                )}
               </div>
+              <button
+                type="submit"
+                className="w-full text-center py-2 rounded-md bg-blue-500 hover:bg-blue-600 cursor-pointer text-white"
+              >
+                Submit
+              </button>
             </form>
           </div>
           <div className="text-black h-full">hello world</div>
