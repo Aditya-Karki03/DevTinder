c2e6dedddbe951e74ca884140da3dca94518af32
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2025-03-03 22:28:17 +0530

    Update Dashboard.tsx

--- frontend/src/pages/dashboard 2/Dashboard.tsx
+++ frontend/src/pages/dashboard 2/Dashboard.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 
+//Delete this file as it is duplicate 
+ //dashboard file
 const Dashboard = () => {
   return (
     <div className="w-screen h-screen flex flex-col">
