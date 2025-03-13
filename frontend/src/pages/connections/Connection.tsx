53f1af0fbf4b80ed29e8d2cba7e7eeac615e4cf9
Aditya Karki <adityakarki201@gmail.com>
2025-03-13 20:44:55 +0530

    Fix: UX

--- frontend/src/pages/connections/Connection.tsx
+++ frontend/src/pages/connections/Connection.tsx
@@ -3,18 +3,30 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllFriendsRequest } from "./slice";
 import { RootState } from "../../redux/store";
 import FriendsProfile from "../../components/FriendsProfile";
+import { Loader2 } from "lucide-react";
 
 const Connection = () => {
   const dispatch = useDispatch();
-  const friends = useSelector((store: RootState) => store?.friends?.friends);
+  const { friends, loading } = useSelector(
+    (store: RootState) => store?.friends
+  );
   useEffect(() => {
     dispatch(getAllFriendsRequest());
   }, [dispatch]);
   return (
     <div className="flex flex-grow flex-col">
-      {friends?.map((friend) => (
-        <FriendsProfile key={friend._id} friend={friend} />
-      ))}
+      {loading ? (
+        <Loader2 className="h-12 w-12 text-white animate-spin m-auto" />
+      ) : friends?.length == 0 ? (
+        <p>
+          You don't have any friends, try swiping first or wait to be swiped
+        </p>
+      ) : (
+        friends?.map((friend) => (
+          <FriendsProfile key={friend._id} friend={friend} />
+        ))
+      )}
+      {}
     </div>
   );
 };
